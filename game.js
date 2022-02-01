a1136e5111165f23d134e4d4323c184b63212f3e
tobuya <thomasobuya95@yahoo.com>
2022-02-01 15:02:09 +0300

    Edit of game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 let computerSelection;
-let playerSelection = "scissors";
+let playerSelection = prompt("Enter your input for the game:");
 let computerScore = 0;
 let playerScore = 0;
 let roundWinner = "";
@@ -8,25 +8,25 @@ myArray = ["Rock", "Paper", "Scissors"]
 function computerPlay() {
     return myArray[Math.floor(Math.random() *  myArray.length)]//[Math.floor(Math.random()*3)]
 }
-console.log(computerPlay())
+//console.log(computerPlay())
 
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerPlay().toLowerCase();
     if(
-        (playerSelection == "rock" && computerSelection == "paper") ||
-        (playerSelection == "scissors" && computerSelection == "rock") ||
-        (playerSelection == "paper" && computerSelection == "scissors")
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "scissors" && computerSelection === "rock") ||
+        (playerSelection === "paper" && computerSelection === "scissors")
        ){
-         computerScore++;
+         computerScore = ++computerScore;
          roundWinner = "Computer";
     }else if(
-             (playerSelection == "paper" && computerSelection == "rock") ||
-             (playerSelection == "rock" && computerSelection == "scissors") ||
-             (playerSelection == "scissors" && computerSelection == "paper")
+             (playerSelection === "paper" && computerSelection === "rock") ||
+             (playerSelection === "rock" && computerSelection === "scissors") ||
+             (playerSelection === "scissors" && computerSelection === "paper")
              ){
-         playerScore++;
+         playerScore = ++playerScore;
          roundWinner = "Player";
     }else {
          roundWinner = "Draw!";
